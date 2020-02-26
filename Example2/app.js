89073c8246607b536c3be596a594eba98a27e4d6
amy444 <amy75080@gmail.com>
2020-02-26 12:49:42 +0530

    First commit

--- Example2/app.js
+++ Example2/app.js
@@ -1,8 +1,16 @@
 var http = require('http');
+var events=require('events');
+
+var eventEmitter=new events.EventEmitter();
 
 var server=http.createServer(function(req, res){
+    eventEmitter.emit('amit requested'); // event trigger
     res.end("server works");
 });
+
+eventEmitter.on('amit requested', function(){
+    console.log('request has been done on server')
+}); //event listener 
 
 server.listen(3000, 'localhost', function(){
     console.log('Server started at 3000');
