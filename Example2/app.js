079f19f590dc427ffb3e3bde8e28e9e0bbeef048
amy444 <amy75080@gmail.com>
2020-02-26 15:32:17 +0530

    Comment

--- Example2/app.js
+++ Example2/app.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 var events=require('events');
 
@@ -14,4 +15,57 @@ eventEmitter.on('someone requested', function(data){
 
 server.listen(3000, 'localhost', function(){
     console.log('Server started at 3000');
+});
+*/
+
+
+
+/*
+//calling http library
+var http =require('http');
+var url= require('url');
+
+//creating sever
+var server =http.createServer(function(req, res){
+
+    //setting content header
+    res.writeHead(200, ('Content Type', 'text/html'));
+    var q= url.parse(req.url, true).query;
+    var txt=q.year + " "+ q.month;
+
+    //send string to response
+    res.end(txt);
+
+});
+
+//assigning 8092 as server listening port
+server.listen(8092);
+*/
+
+var express=require('express');
+var http=require('http');
+var fs=require('fs');
+
+var app=express();
+var server=http.createServer(app); //created server using the basic method buthave all the functionalities of express server
+
+
+app.get('/', function(req, res){
+    res.send('Express works');
+});
+
+app.get('/tasks', function(req, res){
+    fs.readFile('./db.json', function(err, data){
+      //  res.send(data.toString());
+        var tasks=JSON.parse(data.toString()).tasks;
+        res.json(data.toString());
+       
+    });
+    //res.send('<h1>amit  works</h1>'); 
+});
+
+
+
+server.listen(3000, function(){
+    console.log('Server is listening at 3000');
 });
